186cb943135d7ceda979817622c48efff9ac3060
Shivam Chaturvedi <devs4v@gmail.com>
2014-03-03 01:32:55 +0530

    tweaked questions
    added questions and effects and toolbox
    separate file for the create page

--- create/index.php
+++ create/index.php
@@ -10,10 +10,12 @@ require("../res/funcs.php");
 		<meta charset="UTF-8"/>
 		<meta name="author" content="Shivam Chaturvedi -- The Smarter Techie"/>
 		<meta name="keywords" content="beautiful dynamic forms"/>
+		<link rel="stylesheet" href="../css/main.css"/>
+		<link rel="stylesheet" href="create.css"/>
+		<script type="text/javascript" src="../js/main.js"></script>
+		<!-- Plugins -->
 		<script type="text/javascript" src="../js/jquery.js"></script>
 		<script type="text/javascript" src="../js/jquery.color-2.1.2.min.js"></script>
-		<script type="text/javascript" src="../js/main.js"></script>
-		<link rel="stylesheet" href="../css/main.css"/>
 	</head>
 	<body>
 		<div id="container">
@@ -30,12 +32,19 @@ require("../res/funcs.php");
 				</div>
 			</div>
 			<div id="content">
+				<div id="tools">
+					<div id="bgcolor"><input type="button" value="Add a Question"/></div>
+					<div id="bgcolor">Header-color:</div>
+					<div id="bgcolor">Background-color:</div>
+					<div id="bgcolor">Footer-color:</div>
+				</div>
 				<div id="form">
 					<div id="form-header">
 						<div id="form-title">My Form</div>
 					</div>
 					<div id="questions">
 						<div class="question">
+							<div class="delete" title="Delete this Question">Delete</div>
 							<div class="q-title">Question 1</div>
 							<div class="q-detail">The abstract for the question</div>
 							<div class="q-options">
@@ -46,6 +55,7 @@ require("../res/funcs.php");
 						</div>
 						
 						<div class="question">
+							<div class="delete" title="Delete this Question">Delete</div>
 							<div class="q-title">Question 2</div>
 							<div class="q-detail">The abstract for the question</div>
 							<div class="q-options">
@@ -56,29 +66,46 @@ require("../res/funcs.php");
 						</div>
 						
 						<div class="question">
+							<div class="delete" title="Delete this Question">Delete</div>
 							<div class="q-title">Question 3</div>
 							<div class="q-detail">The abstract for the question</div>
 							<div class="q-options">
 								<div class="q-checkboxes">
-									<input type="checkbox" name="q3" disabled="true"/>
+									<input type="checkbox" disabled="true"/><input class="input-boxes" type="text"/>
 								</div>
+								<input type="checkbox" disabled="true"/><input type="button" class="add-check" value="Add a new Option"/>
 							</div>
 						</div>
 						
 						<div class="question">
+							<div class="delete" title="Delete this Question">Delete</div>
 							<div class="q-title">Question 4</div>
 							<div class="q-detail">The abstract for the question</div>
 							<div class="q-options">
 								<div class="q-optionbuttons">
-									<input type="radio" name="q4" disabled="true"/>
+									<input type="radio" disabled="true"/><input class="input-boxes" type="text"/>
 									<!--<input type="radio" name="q4" disabled="true"/>
 									<input type="radio" name="q4" disabled="true"/>-->
 								</div>
+								<input type="radio" disabled="true"/><input type="button" class="add-opt" value="Add a new Option"/>
+							</div>
+						</div>
+						
+						<div class="question-sample">
+							<div class="q-title-sample"><a href="javascript:;">Add a New Question</a></div>
+							<div id="new-q">
+								<input type="button" value="Short text answer"/>
+								<input type="button" value="Long text answer"/>
+								<input type="button" value="Multiple choice (checkboxes)"/>
+								<input type="button" value="Single choice (option buttons)"/>
 							</div>
 						</div>
 					</div>
 					<div id="form-footer">
-						<div id="form-footer-text"></div>
+						<div id="form-footer-text">
+							<div id="powermsg">Powered by "The Smarter Techies"</div>
+							<div>&copy; 2014 The Smarter Techies</div>
+						</div>
 					</div>
 				</div>
 			</div>
